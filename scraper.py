953500bdcb4c3a9d6b7bcc0764384fe14279be8c
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-05-08 13:33:33 -0300

    2 Commit

--- scraper.py
+++ scraper.py
@@ -1,3 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = "https://ge.globo.com/?utm_source=globo.com&utm_medium=menuburger"
+
+resposta = requests.get(url)
+if resposta.status_code == 200:
+    soup = BeautifulSoup(resposta.content, 'html.parser')
+
+    noticias = soup.find_all('a', class_='feed-post-link gui-color-primary gui-color-hover')
+
+    for noticia in noticias:
+        titulo = noticia.find('p').text
+        print(f'Noticia: {titulo}')
